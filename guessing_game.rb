c9652e3ffe9c039387e53eaa985701dd8c7c0e60
John M Buscemi <jmbuscemi@gmail.com>
2015-05-05 14:12:45 -0400

    Random number and guess checker working

---
+++ guessing_game.rb
@@ -0,0 +1,29 @@
+#define method for random number generator
+def random
+  a = rand(1..100)
+  return a
+end
+
+def check_guess(a,b)
+  if a < b
+    puts "Your number is lower than the computers...try again"
+  elsif a > b
+    puts "Your number is higher than the computers...try again"
+  elsif a = b
+    puts "CONGRATULATIONS!! YOU WIN!!"
+  end
+end
+
+#get user input
+puts "Pick a number between 1 and 100: "
+user_num = gets.chomp.to_i
+#number = number.to_i
+puts "You guessed #{number}"
+
+#call random number method
+computer = random
+#computer = computer.to_i
+puts "Computer's number is #{computer}"
+
+#check users guess
+check_guess(user_num,computer)
