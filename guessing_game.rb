913a5fc21881e5ec4cc8c5d61e8c2658053639e6
John M Buscemi <jmbuscemi@gmail.com>
2015-05-05 16:30:56 -0400

    Working 'Normal Mode' code

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,40 +1,65 @@
+#create guess array
+guesses = Array.new
+
 #get user input
 puts "Pick a number between 1 and 100 (5 tries left): "
 user_num = gets.chomp.to_i
-puts "You guessed #{user_num}"
+#puts "You guessed #{user_num}"
+guesses << user_num
 
-#define methods for random number generator
+#random number generator method
 def random
   rand(1..100)
 end
 
-def check_guess(a,b)
+#guess checker method
+def check_guess(a,b,guesses)
   if a < b
-    puts "Your number is lower than the computers...try again"
+    puts "Your number is LOWER than the computer's...try again"
   elsif a > b
-    puts "Your number is higher than the computers...try again"
+    puts "Your number is HIGHER than the computer's...try again"
   elsif a == b
     puts "CONGRATULATIONS!! YOU WIN!!"
     exit
   end
 end
 
+#message if guesses have already been made...not working
+# def inclusion
+#   puts "You made a HUGE mistake...you've already guessed that."
+#   puts "Pick a number between 1 and 100 (#{5-a} tries left): "
+#   user_num = gets.chomp.to_i
+#   return
+# end
+
 #call random number method
 computer = random
-puts "Computer's number = #{computer}"
+#puts "Computer's number = #{computer}"
 
 #check user's guess
 if user_num != computer
   (1..4).each do |a|
-    check_guess(user_num,computer)
+    check_guess(user_num,computer,guesses)
     if a < 4
       puts "Pick a number between 1 and 100 (#{5-a} tries left): "
       user_num = gets.chomp.to_i
+      while guesses.include?(user_num)
+        puts "You made a HUGE mistake...you've already guessed that."
+        puts "Pick a number between 1 and 100 (#{5-a} tries left): "
+        user_num = gets.chomp.to_i
+      end
+      guesses << user_num
     elsif a == 4
       puts "Pick a number between 1 and 100 (LAST TRY!): "
       user_num = gets.chomp.to_i
+      while guesses.include?(user_num)
+        puts "You made a HUGE mistake...you've already guessed that."
+        puts "Pick a number between 1 and 100 (#{5-a} tries left): "
+        user_num = gets.chomp.to_i
+      end
+      guesses << user_num
       if user_num != computer
-        puts "5 tries...YOU LOSE!"
+        puts "You Lose! Here are your lotto numbers...err guesses: #{guesses}"
       elsif user_num == computer
         puts "Congratulations! You got it on the last try."
       end
